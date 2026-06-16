0cccef5e59b6319fbf03b45b7bb314fc9e32954c
alimanpur <aliasgerm71@gmail.com>
2026-06-17 00:01:17 +0530

    fix of URL

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,7 @@ import { seedCategories } from './config/seed.js';
 
 const app = express();
 
-app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: process.env.FRONTEND_URL || 'https://expense-flow-day02.vercel.app', credentials: true }));
 app.use(express.json());
 
 app.use('/api/v1/auth', authRoutes);
